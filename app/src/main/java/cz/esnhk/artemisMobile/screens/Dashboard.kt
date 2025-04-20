d589bc44d3ef2a2a9c8cecddb920813cfb51f97f
Akr1j <jirkajandourek@seznam.cz>
2025-04-20 15:21:25 +0200

    feat: Dashboard screen scrollable

--- app/src/main/java/cz/esnhk/artemisMobile/screens/Dashboard.kt
+++ app/src/main/java/cz/esnhk/artemisMobile/screens/Dashboard.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,8 +24,10 @@ fun Dashboard(
         Text("Dashboard")
         Spacer(modifier = Modifier.height(16.dp))
 
-        studentList.forEach { student ->
-            StudentItem(student, navController)
+        LazyColumn {  // Use LazyColumn
+            items(studentList) { student ->
+                StudentItem(student, navController)
+            }
         }
 
     }
